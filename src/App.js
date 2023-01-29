f3c0cf560e6c54fb9c8fa006df8c3956df1cfbd0
ImMalika <qmalika5450@gmail.com>
2023-01-29 21:54:24 +0500

    for new changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import {Routes, Route} from "react-router-dom";
-import {Home} from './pages/main/home/home';
+import { Routes, Route } from "react-router-dom";
+import { Home } from './pages/main/home/home';
 
 function App() {
   return (
-    
+
+    <div>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        
       </Routes>
+    </div>
 
   );
 }
