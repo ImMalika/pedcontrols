bec175d51e7ddb5f06e2ae9025f0d8703e8b4e49
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-01-31 21:24:38 +0500

    carousel have been added

--- src/components/info/info2/carousel1/carousel1.js
+++ src/components/info/info2/carousel1/carousel1.js
@@ -1,66 +1,62 @@
-import React, { Suspense } from "react";
-import "./insidePages.scss";
-import "../../scss/main.scss";
-import { Nav } from "../../components/navbar/nav";
-import NavbarLinks from "../../components/navbar2/NavbarLinks";
-import Footer from "../../components/footer/footer";
-import ShowBg from "../../components/showBg/showBg";
-import bgImg from "../../media/ship.jpg";
-import shipFreightImg from "../../media/shipping-rates-are-finally-falling-1428282157-standard-1536x1536.jpg";
-import InsideCategories from "./info2Categories/insideCategories";
-import i18next from "i18next";
-import { useTranslation } from "react-i18next";
-const InsideShip = () => {
-	const { t } = useTranslation();
-	const handleClick = (e) => {
-		i18next.changeLanguage(e.target.value);
+import React from "react";
+import "./carousel1.scss";
+import { useState } from "react";
+import Slider from "react-slick";
+import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { Link } from "react-router-dom";
+const Carousel1 = () => {
+	const data = [
+		{
+			img: "https://www.educationperfect.com/wp-content/uploads/2022/11/Generic_Happyhighschoolgirlusinglaptopinclassroom_1500px.webp",
+			title: "img 1",
+		},
+		{
+			img: "https://www.bankofengland.co.uk/-/media/boe/images/education-and-engagement/education/teacher-resource-image-1-web.jpg?h=347&la=en&mh=347&mw=520&w=520&hash=76512627AB76D24E22CFC00C41AD0BA59F624BA9",
+			title: "img 2",
+		},
+		{
+			img: "https://alis.alberta.ca/media/2149/students-in-class.jpg?anchor=center&mode=crop&width=606&height=440&rnd=131868559330000000",
+			title: "img 3",
+		},
+		{
+			img: "https://www.cdc.gov/healthyyouth/protective/images/EDkQGdeXkAIiM0q.jpg?_=56749",
+			title: "img 4",
+		},
+	];
+	const NextArrow = ({ onClick }) => (
+		<div className="arrow next" onClick={onClick}>
+			<FaArrowRight />
+		</div>
+	);
+	const PrevArrow = ({ onClick }) => (
+		<div className="arrow prev" onClick={onClick}>
+			<FaArrowLeft />
+		</div>
+	);
+	const [imageIndex, setImageIndex] = useState(0);
+	const settings = {
+		infinite: true,
+		lazyload: true,
+		speed: 300,
+		slidesToShow: 3,
+		centerMode: true,
+		centerPadding: 0,
+		nextArrow: <NextArrow />,
+		prevArrow: <PrevArrow />,
+		beforeChange: (current, next) => setImageIndex(next),
 	};
 	return (
-		<Suspense fallback="loading">
-			<div className="main inside">
-				<Nav onClick={(e) => handleClick(e)} />
-				<input type="checkbox" id="navLinks__menu" />
-				<NavbarLinks />
-				<label htmlFor="navLinks__menu" className="navLinks__menu"></label>
-				<ShowBg bgImage={bgImg} title={"Our company"} />
-				<h2 className="page__title">
-					{t("service")} <span>/ {t("servicesShipFreightTitle")} </span>
-				</h2>
-				<div className="inside__elements wrapper">
-					<div className="inside__elements-col1">
-						<h3>{t("infoService")}</h3>
-						<InsideCategories />
+		<div className="carousel1">
+			<Slider {...settings}>
+				{data.map((item, index) => (
+					<div className={index === imageIndex ? "slide active" : "slide"}>
+						<img src={item.img} alt={item.img} />
+						<Link to="/cas" className="link">{item.title}</Link>
 					</div>
-					<div className="inside__elements-col2">
-						<div className="inside__elements-col2-elements">
-							<img src={shipFreightImg} alt="inside img" />
-							<p>
-								{t("servicesShipFreightDescription")}
-								<p>{t("servicesShipFreightDescription1")}</p>
-								<p>{t("servicesShipFreightDescription2")}</p>
-								<p>{t("servicesShipFreightDescription3")}</p>
-								<p>{t("servicesShipFreightDescription4")}</p>
-								<p>{t("servicesShipFreightDescription5")}</p>
-								<p>{t("servicesShipFreightDescription6")}</p>
-								<p>{t("servicesShipFreightDescription7")}</p>
-								<p>{t("servicesShipFreightDescription8")}</p>
-								<p>{t("servicesShipFreightDescription9")}</p>
-								<p>{t("servicesShipFreightDescription10")}</p>
-								<p>{t("servicesShipFreightDescription11")}</p>
-								<p>{t("servicesShipFreightDescription12")}</p>
-								<p>{t("servicesShipFreightDescription13")}</p>
-								<p>{t("servicesShipFreightDescription14")}</p>
-								<p>{t("servicesShipFreightDescription15")}</p>
-								<p>{t("servicesShipFreightDescription16")}</p>
-								<p>{t("servicesShipFreightDescription17")}</p>
-								<p>{t("servicesShipFreightDescription18")}</p>
-							</p>
-						</div>
-					</div>
-				</div>
-				<Footer />
-			</div>
-		</Suspense>
+				))}
+			</Slider>
+		</div>
 	);
 };
-export { InsideShip };
+
+export default Carousel1;
