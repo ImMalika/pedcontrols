48690240298dd20bc05ccf179289a2eb218b653d
ImMalika <qmalika5450@gmail.com>
2023-02-02 00:37:20 +0500

    navbar was activated

---
+++ src/components/layout/layout.js
@@ -0,0 +1,16 @@
+import Navbar from "../navbar/nav";
+import Footer from "../footer/footer";
+import Home from "../../pages/main/home/home";
+import AboutUs from "../../pages/main/aboutUs/aboutUs";
+import Study from "../../pages/main/study/study";
+
+export default function Layout() {
+  return (
+    <div>
+      <Navbar />
+        
+
+      <Footer />
+    </div>
+  );
+}
--- src/components/navMain/navLinks.js
+++ src/components/navMain/navLinks.js
@@ -1,21 +1,21 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import { NavLink} from "react-router-dom";
 
 const NavLinks = () => {
 	return (
 		<div className="navMain__menu-links">
-			<Link to={""} className="navMain__menu-links-link">
+			<NavLink to={""} className="navMain__menu-links-link">
 				Biz haqimizda
-			</Link>
-			<Link to={""} className="navMain__menu-links-link">
+			</NavLink>
+			<NavLink to={""} className="navMain__menu-links-link">
 				Ota-onalar uchun
-			</Link>
-			<Link to={""} className="navMain__menu-links-link">
+			</NavLink>
+			<NavLink to={""} className="navMain__menu-links-link">
 				O'smirlar uchun
-			</Link>
-			<Link to={""} className="navMain__menu-links-link">
+			</NavLink>
+			<NavLink to={""} className="navMain__menu-links-link">
 				Mo'dulli o'quv dastur
-			</Link>
+			</NavLink>
 		</div>
 	);
 };
