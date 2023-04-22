90c9124a94f370b225f344de59e77a9afbd2b891
ImMalika <qmalika5450@gmail.com>
2023-04-22 23:08:22 +0500

    data were added

--- src/pages/parents/parentsInfo1/parentsInfo.js
+++ src/pages/parents/parentsInfo1/parentsInfo.js
@@ -18,13 +18,47 @@ const ParentsInfo = () => {
 						<h1 className="parentsInfo__titles-title">{item.title}</h1>
 						<img src={item.img} alt="" />
 						<div>
-							<p>{item.description}</p><br />
-							<p>{item.description2}</p><br />
-							<p>{item.description3}</p><br />
-							<p>{item.description4}</p><br />
-							<h3>{item.title2}</h3>
-							<p>{item.description5}</p>
+						<p><b>{item.question1}</b>{item.description}</p><br />
+						<p><b>{item.answer1}</b>{item.answerdesc1}</p><br />
+						<p><b>{item.question2}</b>{item.description2}</p><br />
+						<p><b>{item.answer2}</b>{item.answerdesc2}</p><br />
+						<p><b>{item.question3}</b>{item.description3}</p><br />
+						<p><b>{item.answer3}</b>{item.answerdesc3}</p>
+						<p>{item.answerdesc3a}</p>
+						<p>{item.answerdesc3b}</p>
+						<p>{item.answerdesc3c}</p>
+						<p>{item.answerdesc3d}</p>
+						<br />
+						<p><b>{item.question4}</b>{item.description4}</p><br />
+						<p><b>{item.answer4}</b>{item.answerdesc4}</p><br />
+						<p><b>{item.question5}</b>{item.description5}</p><br />
+						<p><b>{item.answer5}</b>{item.answerdesc5}</p><br />
+						<p><b>{item.question6}</b>{item.description6}</p><br />
+						<p><b>{item.answer6}</b>{item.answerdesc6}</p><br />
+						<p><b>{item.question7}</b>{item.description7}</p>
+						<p>{item.description7a}</p>
+						<br />
+						<p><b>{item.answer7}</b>{item.answerdesc7}</p>
+						<br />
+						<p><b>{item.question8}</b>{item.description8}</p>
+						<p>{item.description8a}</p><br />
+						<p><b>{item.answer8}</b>{item.answerdesc8}</p>
+						<p>{item.answerdesc8}</p><br />
+						<p><b>{item.question9}</b>{item.description9}</p><br />
+						<p><b>{item.answer9}</b>{item.answerdesc9}</p><br />
+						<p><b>{item.question10}</b>{item.description10}</p><br />
+						<p><b>{item.answer10}</b>{item.answerdesc10}</p><br />
+						<p><b>{item.question11}</b>{item.description11}</p><br />
+						<p><b>{item.answer11}</b>{item.answerdesc11}</p><br />
+						<p><b>{item.question12}</b>{item.description12}</p>
+						<p>{item.description12a}</p><br />
+						<p><b>{item.answer12}</b>{item.answerdesc12}</p><br />
+
+						<p><b>{item.advicequestion}</b>{item.advicequestion1}</p><br />
+							<p><b>{item.adviceanswer1}</b> {item.advicedesc1}</p>
 						</div>
+
+						
 					</div>
 				) : null
 			)}
