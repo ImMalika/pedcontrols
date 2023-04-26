ff7c369acb2239a15ae8f87fa1b6a51c29bcfde8
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-04-26 19:45:54 +0500

    carousel updated

--- src/components/infoCarousels/carousel1/carousel1.js
+++ src/components/infoCarousels/carousel1/carousel1.js
@@ -3,11 +3,12 @@ import "./carousel1.scss";
 import { useState } from "react";
 import Slider from "react-slick";
 import { FaArrowRight, FaArrowLeft } from "react-icons/fa";
+import { FaArrowCircleRight, FaArrowCircleLeft } from "react-icons/fa";
 import { Link } from "react-router-dom";
 const Carousel1 = () => {
 	const data = [
 		{
- 			img: "https://www.bankofengland.co.uk/-/media/boe/images/education-and-engagement/education/teacher-resource-image-1-web.jpg?h=347&la=en&mh=347&mw=520&w=520&hash=76512627AB76D24E22CFC00C41AD0BA59F624BA9",
+			img: "https://www.bankofengland.co.uk/-/media/boe/images/education-and-engagement/education/teacher-resource-image-1-web.jpg?h=347&la=en&mh=347&mw=520&w=520&hash=76512627AB76D24E22CFC00C41AD0BA59F624BA9",
 			title: "img 1",
 			link: "/study",
 		},
@@ -26,15 +27,30 @@ const Carousel1 = () => {
 			title: "img 4",
 			link: "/study",
 		},
+		{
+			img: "https://www.cdc.gov/healthyyouth/protective/images/EDkQGdeXkAIiM0q.jpg?_=56749",
+			title: "img 4",
+			link: "/study",
+		},
+		{
+			img: "https://www.cdc.gov/healthyyouth/protective/images/EDkQGdeXkAIiM0q.jpg?_=56749",
+			title: "img 4",
+			link: "/study",
+		},
+		{
+			img: "https://www.cdc.gov/healthyyouth/protective/images/EDkQGdeXkAIiM0q.jpg?_=56749",
+			title: "img 4",
+			link: "/study",
+		},
 	];
 	const NextArrow = ({ onClick }) => (
 		<div className="arrow next" onClick={onClick}>
-			<FaArrowRight />
+			<FaArrowCircleRight />
 		</div>
 	);
 	const PrevArrow = ({ onClick }) => (
 		<div className="arrow prev" onClick={onClick}>
-			<FaArrowLeft />
+			<FaArrowCircleLeft />
 		</div>
 	);
 	const [imageIndex, setImageIndex] = useState(0);
@@ -42,7 +58,7 @@ const Carousel1 = () => {
 		infinite: true,
 		lazyload: true,
 		speed: 300,
-		slidesToShow: 3,
+		slidesToShow: 5,
 		centerMode: true,
 		centerPadding: 0,
 		nextArrow: <NextArrow />,
@@ -53,7 +69,10 @@ const Carousel1 = () => {
 		<div className="carousel1">
 			<Slider {...settings}>
 				{data.map((item, index) => (
-					<div key={index} className={index === imageIndex ? "slide active" : "slide"}>
+					<div
+						key={index}
+						className={index === imageIndex ? "slide active" : "slide"}
+					>
 						<img src={item.img} alt={item.img} />
 						<Link to={item.link} className="link">
 							{item.title}
