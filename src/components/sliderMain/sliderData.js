d773430a35bffca303a6b803974ea62e4adc0af1
ImMalika <qmalika5450@gmail.com>
2023-04-28 00:26:03 +0500

    slider images were changed

--- src/components/sliderMain/sliderData.js
+++ src/components/sliderMain/sliderData.js
@@ -1,35 +1,35 @@
-import img1 from "../../media/1.jpg";
-import img1Oila from "../../media/oila.jpg";
-import img2 from "../../media/2.jpg";
-import img3 from "../../media/3.1.jpg";
-import img4 from "../../media/4.jpg";
-import img4Family_Portrait from "../../media/Family_Portrait.jpg";
+import img1 from "../../media/slider/1.jpg";
+// import img1Oila from "../../media/oila.jpg";
+import img2 from "../../media/slider/2.1.jpg";
+import img3 from "../../media/slider/3.1.jpg";
+import img4 from "../../media/slider/ogohlik.jpg";
+// import img4Family_Portrait from "../../media/Family_Portrait.jpg";
 
 export const sliderData = [
 	{
 		title: "Oila muqaddasdir",
 		description: "this is the first slider image",
-		// url: img1, // Buni kommentga oling
-		url: "https://thumbs.dreamstime.com/z/well-done-attentive-longhaired-female-sitting-grass-her-daughter-to-complete-task-positive-delighted-people-drawing-their-129099859.jpg", // Mana shuni kommentdan chiqaring
+		url: img1, // Buni kommentga oling
+		// url: "https://thumbs.dreamstime.com/z/well-done-attentive-longhaired-female-sitting-grass-her-daughter-to-complete-task-positive-delighted-people-drawing-their-129099859.jpg", // Mana shuni kommentdan chiqaring
 	},
 	{
-		title: "Giyohvandlik – umr zavoli",
-		description: "this is the second slider image",
+		// title: "Giyohvandlik – umr zavoli",
+		// description: "this is the second slider image",
 		url: img2,
 	},
 	{
-		title: "Ijtimoy tarmoqning 'qarmoqlari' ",
-		description: "this is the third slider image",
+		// title: "Ijtimoy tarmoqning 'qarmoqlari' ",
+		// description: "this is the third slider image",
 		url: img3,
 	},
 	{
-		title: "Biz barchamiz - ogohmiz!",
-		description: "this is the fourth slider image",
+		// title: "Biz barchamiz - ogohmiz!",
+		// description: "this is the fourth slider image",
 
-		// url: img4, // Buni kommentga oling
+		url: img4, // Buni kommentga oling
 
-		url: img4Family_Portrait, // Mana shuni kommentdan chiqaring
+		// url: img4Family_Portrait, // Mana shuni kommentdan chiqaring
 
-		url: img1, // Mana shuni kommentdan chiqaring
+		// url: img1, // Mana shuni kommentdan chiqaring
 	},
 ]; 
