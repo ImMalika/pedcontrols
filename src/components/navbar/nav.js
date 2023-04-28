1d4008815a9b518178201419cfadbcb6bac4f216
hojimurodtoshqulov <hojimurod2603@gmail.com>
2023-04-28 23:12:50 +0500

    links updated

--- src/components/navbar/nav.js
+++ src/components/navbar/nav.js
@@ -11,6 +11,9 @@ const onClick = ({ key }) => {
 
 const Nav = ({ dataLink }) => {
 	// console.log("items", dataLink);
+	const scrollToTop = () => {
+		window.scrollTo(0, 0);
+	};
 	const [navSize, setnavSize] = useState("13vh");
 	const listenScrollEvent = () => {
 		window.scrollY > 150 ? setnavSize("9vh") : setnavSize("12vh");
@@ -30,14 +33,18 @@ const Nav = ({ dataLink }) => {
 				transition: "all 0.8s",
 			}}
 		>
-			<Link className="nav__logo" to="/">
+			<Link className="nav__logo" to="/" onClick={scrollToTop}>
 				<img className="nav__logo-img" src={navLogo} alt="nav logo" />
 				<h4>PEDCONTROLS.UZ</h4>
 			</Link>
 			<div className="mainDropDown nav__links">
 				{dataLink.map((item, index) => (
-					<div key={index} className="nav__links-div">
-						<NavLink className="nav__links-div-link" to={`${item.link}`}>
+					<div key={index} className="nav__links-div" onClick={scrollToTop}>
+						<NavLink
+							className="nav__links-div-link"
+							to={`${item.link}`}
+							onClick={scrollToTop}
+						>
 							<div className="nav__links-div-line top"></div>
 							<p>
 								{item.title}{" "}
@@ -51,15 +58,17 @@ const Nav = ({ dataLink }) => {
 									? "nav__links-div-dropDown"
 									: "nav__links-div-dropDown dropDownNone"
 							}
+							onClick={scrollToTop}
 						>
 							{item.dropDown !== undefined
 								? item.dropDown?.map((item, index) => (
 										<div
 											key={index}
 											className={"nav__links-div-dropDown-drLink"}
+											onClick={scrollToTop}
 										>
 											{/* {console.log("item-InDropDown>>>", item.inDropDown)} */}
-											<Link to={item.link}>
+											<Link to={item.link} onClick={scrollToTop}>
 												{item.title}{" "}
 												{item.inDropDown !== undefined ? (
 													<DownOutlined />
@@ -71,11 +80,16 @@ const Nav = ({ dataLink }) => {
 														? "nav__links-div-dropDown-drLink-div"
 														: "nav__links-div-dropDown-drLink-div drLinksNone"
 												}
+												onClick={scrollToTop}
 											>
 												{item.inDropDown !== undefined
 													? item.inDropDown?.map((item, index) => (
-															<Link to={item.link} key={index}>
-																{console.log("item --> ", item.link)}
+															<Link
+																to={item.link}
+																key={index}
+																onClick={scrollToTop}
+															>
+																{/* {console.log("item --> ", item.link)} */}
 																{item.title}
 															</Link>
 													  ))
