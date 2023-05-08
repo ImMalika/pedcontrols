8bd1cf66dd93ee5784677436a89f968ddb70f03a
ImMalika <qmalika5450@gmail.com>
2023-05-08 16:45:18 +0500

    book's images were changed

--- src/components/advantages/advantages.js
+++ src/components/advantages/advantages.js
@@ -10,12 +10,9 @@ const Advantages = () => {
 					src="https://maktab.uz/img/092424ebe03cc1c764d15dcae28c0f1b.png"
 					alt=""
 				/>
-				<h2>Ro‘yxatdan o‘tish juda oson!</h2>
+				<h2>Modulli o‘quv dasturlari!</h2>
 				<p>
-					Platforma imkoniyatlaridan foydalanish uchun saytdan ro‘yxatdan o‘tish
-					lozim. Bu esa ko‘p vaqt olmaydi. Ro‘yxatdan o‘tish tugmasi orqali
-					formani to‘ldiring va bir necha daqiqada platforma foydalanuvchilari
-					qatoridan joy oling!
+				Platformada o‘smir yoshidagi bolalarning bo‘sh vaqtlarni mazmunli tashkil etish maqsadida modulli o‘quv dasturlari doir hushyorlik va ogohlik fazilatlarini shakllantirish ta’limiy texnologiyalarlar yaratilgan bo‘lib, mustaqil ta’lim shaklida yo‘lga qo‘yilganligi ham yoshlarimiz uchun yangi imkoniyatlar eshigini ochadi. 
 				</p>
 			</div>
 			<div className="advantages__card">
@@ -23,12 +20,9 @@ const Advantages = () => {
 					src="https://maktab.uz/img/6a11724df9be893998cf4b6051b7c8cd.png"
 					alt=""
 				/>
-				<h2>Siz kutgan yo‘nalishlar!</h2>
+				<h2>Ogohlik va hushyorlik!</h2>
 				<p>
-					Bizning videodarslar Siz o‘rganishni istagan yo‘nalishlarni qamrab
-					oladi. O‘quv dasturidan tashqari, zamonaviy bilimlarga ham urg‘u
-					berdik. Bu hali hammasi emas. Maktab.uz yana yangidan-yangi
-					videodarslar bilan boyitib boriladi.
+				Bizning platformamiz hushyorlik va ogohlik fazilatlarini shakllantirish doir axborotlarni qamrab oladi. “Internetga qaramlik”, “Onlayn qimor”, onlayn savdo maydonidagi firibgarlik jinoyatlari va tahdidlardan himoyalanishni ta’minlash masalasiga doir ma’lumotlar keltirilgan.
 				</p>
 			</div>
 			<div className="advantages__card">
@@ -36,12 +30,9 @@ const Advantages = () => {
 					src="https://maktab.uz/img/eeb279ebec5825e5a6638470698f3c0c.png"
 					alt=""
 				/>
-				<h2>Qiziqarli va foydali kontent!</h2>
+				<h2>Foydali kontentlar!</h2>
 				<p>
-					Maktab.uz saytiga xalqaro hamkorlar taqdim etgan interfaol
-					videodarslar bilan birga mahalliy mutaxassislar va tashkilotlar bilan
-					hamkorlikda ishlab chiqilgan sermazmun videoroliklar ham jamlanmoqda.
-					Asosiysi, ushbu foydali kontent hech biringizni qiziqtirmay qo‘ymaydi.
+				pedcontrols.uz platformasiga xorijiy hamkorlar taqdim etgan interfaol videodarslar bilan birga mahalliy mutaxassislar va tashkilotlar bilan hamkorlikda ishlab chiqilgan sermazmun videoroliklar ham jamlanmoqda. Asosiysi, ushbu foydali kontent hech biringizni qiziqtirmay qo‘ymaydi.
 				</p>
 			</div>
 		</div>
